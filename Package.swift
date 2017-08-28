b5790b72f2c85b4d961a8f90014ae020a92d0aa0
John Holdsworth <github@johnholdsworth.com>
2017-08-28 12:44:18 +0100

    For testing on Android

--- Package.swift
+++ Package.swift
@@ -1,3 +1,19 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
-let package = Package(name: "XCTest")
+let package = Package(
+    name: "XCTest",
+    products:[
+        .library(
+            name: "XCTest",
+            type: .dynamic,
+            targets:["XCTest"]
+        )
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(name: "XCTest", dependencies: [], path: "Sources"),
+        ]
+)
